b73fd7e9c97dd095cc80f8ad63099e82a87f5ec5
Manickam <manickam.g@busofttech.com>
2020-07-15 18:44:06 +0530

    Add API.

--- src/App.js
+++ src/App.js
@@ -6,16 +6,36 @@ import reducer from "store/reducer";
 import "assets/vendors/style";
 import MainApp from "containers/App";
 
+
+// AWS Amplify Config
+import Amplify, { API } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+function getData() { 
+  const apiName = 'api';
+  const path = '/groids';
+  const myInit = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  return API.get(apiName, path, myInit);
+}
+
+
 const createBrowserHistory = require('history').createBrowserHistory;
 export const history = createBrowserHistory();
-
-
-
-
 
 const App = () => {
   const initialState = useContext(Context);
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  (async function () {
+    const response = await getData();
+    console.log(response);
+  })();
+
   return (
     <BrowserRouter>
       <Context.Provider value={{ state, dispatch }} >
