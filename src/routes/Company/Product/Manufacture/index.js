0d6d15692a6a5627375ef31735447481d8031842
Manickam <manickam.g@busofttech.com>
2020-07-17 23:00:22 +0530

    Controller Name change.

--- src/routes/Company/Product/Manufacture/index.js
+++ src/routes/Company/Product/Manufacture/index.js
@@ -1,24 +1,24 @@
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import AppModuleHeader from "components/AppModuleHeader/index";
 import { Button, Row, Col } from "antd";
 import ListView from './view';
 import ListForm from './form';
-import {GetManufacturesAPI} from 'api/controller/Shared/ManufactureController'
+import { GetManufacturesAPI } from 'api/Controller/Shared/ManufactureController'
 
-const filter ={status:true}
+const filter = { status: true }
 function Manufacture() {
     const [view, setView] = useState(true);
-    const [manufactures,setManufactures] = useState([]);
+    const [manufactures, setManufactures] = useState([]);
     const viewChanged = () => {
         setView(!view)
     }
 
     useEffect(() => {
-        GetManufacturesAPI(filter , (data,err)=>{
+        GetManufacturesAPI(filter, (data, err) => {
             console.log(data)
             setManufactures(data)
         })
-      }, [GetManufacturesAPI]);
+    }, [GetManufacturesAPI]);
 
     return (
         <div className="gx-module-box-content">
@@ -31,7 +31,7 @@ function Manufacture() {
                     <Col>
                         <Button className="gx-btn-block ant-btn" type="primary" aria-label="add" onClick={viewChanged}>
                             {view ? (
-                                <><i className="icon icon-add-circle gx-fs-lg gx-d-inline-flex gx-vertical-align-middle"/>
+                                <><i className="icon icon-add-circle gx-fs-lg gx-d-inline-flex gx-vertical-align-middle" />
                                     <span>Add New Manufacture</span></>) : (
                                     <>
                                         <i className="icon icon-eye gx-mr-2" />
@@ -41,7 +41,7 @@ function Manufacture() {
                     </Col>
                 </Row>
             </div>
-                {view ? (<ListView  data={manufactures}/>) : <ListForm />}
+            {view ? (<ListView data={manufactures} />) : <ListForm />}
 
         </div>
     )
