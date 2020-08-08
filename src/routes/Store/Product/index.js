d3b78c84c080f78b8cb0ee53b1628760611c77ef
Manickam <manickam.g@busofttech.com>
2020-08-08 17:12:22 +0530

    Change Password API Integrated.

--- src/routes/Store/Product/index.js
+++ src/routes/Store/Product/index.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Divider, Input } from 'antd';
+const Search = Input.Search;
+
+const PageHeaderWithBack = ({ title, subtitle, backurl }) =>
+    <div>
+        <h2>
+            {title}
+        </h2>
+        <span className={'badge'}>{subtitle}</span>
+        <Divider orientation="left" plain>
+        </Divider>
+    </div>;
 
 const ProductList = () => {
     return (
-        <div>
-            ProductList
-        </div>
+        <>
+            <PageHeaderWithBack title="Product Name" subtitle="Product" backurl=""></PageHeaderWithBack>
+            <Search placeholder="What you like..."
+             style={{ width: '50%',
+             position: 'absolute',
+             top: '33px',
+             left: '250px' }}
+             enterButton="Search Product" size="large"/>
+        </>
     )
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ import {Route, Switch} from "react-router-dom";
 
 
 import Main from "./main/index";
-import Product from "./Company/Product"
-import Stores from "./Company/Stores"
-import Settings from "./Company/Setttings"
-import UnitTest from "./UnitTest"
+import Product from "./Company/Product";
+import Stores from "./Company/Stores";
+import Settings from "./Company/Setttings";
+import UnitTest from "./UnitTest";
+
+import Store_Product from "./Store/Product"
 
 const App = ({match}) => (
   <div className="gx-main-content-wrapper">
@@ -16,6 +18,7 @@ const App = ({match}) => (
       <Route path={`${match.url}stores`} component={Stores}/>
       <Route path={`${match.url}settings`} component={Settings}/>
       <Route path={`${match.url}unittest`} component={UnitTest}/>
+      <Route path={`${match.url}store/Product`} component={Store_Product}/>
     </Switch>
   </div>
 );
